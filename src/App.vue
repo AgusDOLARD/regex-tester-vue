15f0dd0695c347fedefa9b0edc7c02413b90d7d1
AgusDOLARD <agust_22_10@hotmail.com>
2021-08-24 17:30:03 -0300

    all I see is 🔥

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Regex Tester</h1>
+  <Tester />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tester from "./components/Tester.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Tester,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url("~simpledotcss/simple.min.css");
 </style>
---
+++ src/components/Tester.vue
@@ -0,0 +1,68 @@
+<template>
+  <label for="regex">Regular Expression</label>
+  <input @keyup="testit" v-model="regex" type="text" name="regex" id="regex" />
+  <label for="userInput">Test String</label>
+  <textarea
+    @keyup="testit"
+    rows="5"
+    v-model="userInput"
+    type="text"
+    name="userInput"
+    id="userInput"
+  />
+  <div id="results" v-if="results">
+    <div v-for="result in results" :key="result">
+      <p v-html="result"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "#Testit",
+  setup() {
+    const regex = ref("");
+    const userInput = ref("");
+    const results = ref(null);
+    const testit = () => {
+      const regexval = new RegExp(`(${regex.value})`);
+      results.value = userInput.value.split("\n").map((line) => {
+        return line.replace(regexval, '<span class="highlight">$&</span>');
+      });
+    };
+
+    return {
+      testit,
+      regex,
+      userInput,
+      results,
+    };
+  },
+};
+</script>
+
+<style scoped>
+input,
+textarea,
+label {
+  display: block;
+}
+textarea,
+input {
+  margin: 10px 0;
+  width: 100%;
+}
+label,
+#results {
+  margin-top: 40px;
+}
+#results {
+  padding: 3px 40px;
+  background-color: var(--accent-bg);
+}
+p >>> .highlight {
+  color: var(--accent) !important;
+}
+</style>
